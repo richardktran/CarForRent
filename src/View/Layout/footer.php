87d444f847834a095cdd7442315b067f89b2005e
Dang Khoa Tran <richardktran.dev@gmail.com>
2022-05-18 17:46:14 +0700

    feat: add home page

--- src/View/Layout/footer.php
+++ src/View/Layout/footer.php
@@ -1,5 +1,8 @@
 </body>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/holder.min.js"></script>
+
 </html>
