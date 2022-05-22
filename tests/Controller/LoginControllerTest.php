b9de9c59e5bd0d64c52e123394baee3b4749d658
Tran Dang Khoa <richardktran.dev@gmail.com>
2022-05-23 00:06:49 +0700

    feat: add test for login controller

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -2,12 +2,81 @@
 
 namespace Khoatran\Tests\Controller;
 
+use Khoatran\CarForRent\Controller\LoginController;
+use Khoatran\CarForRent\Exception\LoginException;
+use Khoatran\CarForRent\Http\Request;
+use Khoatran\CarForRent\Http\Response;
+use Khoatran\CarForRent\Model\UserModel;
+use Khoatran\CarForRent\Service\Contracts\LoginServiceInterface;
+use Khoatran\CarForRent\Service\Contracts\SessionServiceInterface;
 use PHPUnit\Framework\TestCase;
 
 class LoginControllerTest extends TestCase
 {
-    public function testDemoAction()
+    protected UserModel $user;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
-        $this->assertEquals(true, true);
+        parent::__construct($name, $data, $dataName);
+        $this->user = new UserModel();
+        $this->user->setId(1);
+        $this->user->setUsername('admin');
+        $this->user->setPassword('12345678');
     }
+
+    public function testLoginActionSuccess(): void
+    {
+        $requestMock = $this->getMockBuilder(Request::class)->getMock();
+        $requestMock->expects($this->once())->method('getBody')->willReturn(['username' => 'admin', 'password' => '12345678']);
+        $response = new Response();
+        $loginServiceMock = $this->getMockBuilder(LoginServiceInterface::class)->disableOriginalConstructor()->getMock();
+        $loginServiceMock->expects($this->once())->method('login')->willReturn($this->user);
+
+        $sessionServiceMock = $this->getMockBuilder(SessionServiceInterface::class)->disableOriginalConstructor()->getMock();
+
+        $loginController = new LoginController($requestMock, $response, $loginServiceMock, $sessionServiceMock);
+        $loginController = $loginController->login();
+
+        $expectedResult = new Response();
+        $expectedResult->setRedirectUrl('/');
+
+        $this->assertEquals($expectedResult, $loginController);
+    }
+
+    public function testLoginActionWithInvalidationBody(): void
+    {
+        $requestMock = $this->getMockBuilder(Request::class)->getMock();
+        $requestMock->expects($this->once())->method('getBody')->willReturn(['username' => '', 'password' => '']);
+        $response = new Response();
+        $loginServiceMock = $this->getMockBuilder(LoginServiceInterface::class)->disableOriginalConstructor()->getMock();
+        $sessionServiceMock = $this->getMockBuilder(SessionServiceInterface::class)->disableOriginalConstructor()->getMock();
+
+        $loginController = new LoginController($requestMock, $response, $loginServiceMock, $sessionServiceMock);
+        $loginController = $loginController->login();
+
+        $expectedResult = new Response();
+        $expectedResult->setTemplate('login');
+
+        $this->assertEquals($expectedResult->getTemplate(), $loginController->getTemplate());
+    }
+
+    public function testLoginActionWithLoginFail(): void
+    {
+        $requestMock = $this->getMockBuilder(Request::class)->getMock();
+        $requestMock->expects($this->once())->method('getBody')->willReturn(['username' => 'admin', 'password' => '12345678']);
+        $response = new Response();
+        $loginServiceMock = $this->getMockBuilder(LoginServiceInterface::class)->disableOriginalConstructor()->getMock();
+        $loginServiceMock->expects($this->once())->method('login')->willThrowException(new LoginException('Your username or password is not correct'));
+        $sessionServiceMock = $this->getMockBuilder(SessionServiceInterface::class)->disableOriginalConstructor()->getMock();
+
+        $loginController = new LoginController($requestMock, $response, $loginServiceMock, $sessionServiceMock);
+        $loginController = $loginController->login();
+
+        $expectedResult = new Response();
+        $expectedResult->setTemplate('login');
+
+        $this->assertEquals($expectedResult->getTemplate(), $loginController->getTemplate());
+    }
+
+
 }
