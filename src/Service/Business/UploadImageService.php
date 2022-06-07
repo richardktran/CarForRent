fa6ef2b685180a5f683648f9b8223de85d70abf1
Dang Khoa Tran <richardktran.dev@gmail.com>
2022-06-07 16:20:27 +0700

    refactor: clean code for UploadImageService

--- src/Service/Business/UploadImageService.php
+++ src/Service/Business/UploadImageService.php
@@ -2,7 +2,6 @@
 
 namespace Khoatran\CarForRent\Service\Business;
 
-use Aws\S3\Exception\S3Exception;
 use Aws\S3\S3Client;
 use Khoatran\CarForRent\Exception\UploadFileException;
 
@@ -15,6 +14,18 @@ class UploadImageService
      */
     public function upload($file): ?string
     {
+        if (!move_uploaded_file($file["tmp_name"], $this->getFileName($file))) {
+            throw new UploadFileException("Error: There was an error uploading your file.");
+        }
+
+        $filePath = $this->getFileName($file);
+
+        $result = $this->uploadS3Service($filePath);
+        return $result->get('ObjectURL');
+    }
+
+    private function uploadS3Service($filePath): \Aws\Result
+    {
         $bucketName = getenv('S3_BUCKET_NAME');
         $bucketRegion = getenv('S3_BUCKET_REGION');
 
@@ -23,25 +34,20 @@ class UploadImageService
             'region' => $bucketRegion,
             'credentials' => ['key' => getenv('S3_ACCESS_KEY_ID'), 'secret' => getenv('S3_SECRET_ACCESS_KEY')]
         ]);
+        $key = basename($filePath);
+        $result = $s3Client->putObject([
+            'Bucket' => $bucketName,
+            'Key' => $key,
+            'SourceFile' => $filePath,
+        ]);
+        unlink($filePath);
+        return $result;
+    }
+
+    private function getFileName($file): string
+    {
         $path = __DIR__ . "/../../../public/upload/";
         $filename = md5(date('Y-m-d H:i:s:u')) . $file["name"];
-
-        if (move_uploaded_file($file["tmp_name"], $path . $filename)) {
-            $file_Path = $path . $filename;
-            $key = basename($file_Path);
-            try {
-                $result = $s3Client->putObject([
-                    'Bucket' => $bucketName,
-                    'Key' => $key,
-                    'SourceFile' => $file_Path,
-                ]);
-                unlink($path . $filename);
-                return $result->get('ObjectURL');
-            } catch (S3Exception $e) {
-                return null;
-            }
-        } else {
-            throw new UploadFileException("Error: There was an error uploading your file.");
-        }
+        return $path . $filename;
     }
 }
